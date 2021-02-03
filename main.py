e055e269753ccecc9fd0c8f012da38d3c5b80020
Nathan <nathan.gt.protic@gmail.com>
2021-02-02 19:52:25 -0500

    update

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 # coding=utf-8
+# Program created by Nathan Gueissaz-Teufel on 2021/02/02
+# https://github.com/nathan-gt/DocxEditPy.git
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import re
+import json
+import os
 from docx import Document
 
 # dictionary
 # the index is the string replaced in the document, it's value is what will it be replaced with
-dict = {
-    "Titre1": "Voici le premier titre",
-    "Titre2": "Voici le premier titre",
-    "Texte": "Du texte",
-    "Gras": "Du texte gras",
-    "Italique": "Du texte italique",
-    "Grand": "Du texte grand"
-}
+with open('src/dictionary.json') as f:
+    dictionary = json.load(f)
 
 
 # function searching through the document given and replaces the string by another
@@ -39,11 +37,16 @@ Tk().withdraw()
 filename = askopenfilename()
 doc = Document(filename)
 
+
 # Loops through dictionary and file
 # Code taken from stackoverflow user szum https://stackoverflow.com/a/42829667/13450363
-for word, replacement in dict.items():
+for word, replacement in dictionary.items():
     word_re = re.compile(word)
     docx_replace_regex(doc, word_re, replacement)
 
+
 # saves the file in current directory
+if os.path.exists("result1.docx"):
+    os.remove("result1.docx")
 doc.save('result1.docx')
+os.system('start result1.docx')
